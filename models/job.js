f65a421a04b0ae035b99dc1bf1a4b2aa03cf758d
Dipesh B C <bcdipeshwork@gmail.com>
2023-11-12 09:19:50 -0500

    Implement job model

--- models/job.js
+++ models/job.js
@@ -46,6 +46,86 @@ class Job {
    */
 
   static async findAll() {
-    const jobsRes = await db.query();
+    const jobsRes = await db.query(
+      `SELECT
+      id, title, salary, equity, company_handle AS "companyHandle"
+      FROM jobs
+      ORDER BY title`
+    );
+
+    return jobsRes.rows;
+  }
+
+  /** Given a id of the job, return data about the job.
+   *
+   * Returns { id, title, salary, equity, companyHandle }
+   * where companyHandle is { handle, name, description, numEmployees, logoUrl }
+   *
+   * Throws NotFoundError if not found.
+   */
+
+  static async get(id) {
+    const jobRes = await db.query(
+      `SELECT
+      id, title, salary, equity, company_handle AS "companyHandle",
+      handle, name, description, num_employees AS "numEmployees", logo_url AS "logoUrl"
+      FROM jobs INNER JOIN companies jobs.companyHandle = companies.handle
+      WHERE id = $1`,
+      [id]
+    );
+
+    const job = jobRes.rows[0];
+
+    if (!job) throw new NotFoundError(`No job: ${id}`);
+
+    return job;
+  }
+
+  /** Update job data with `data`.
+   *
+   * This is a "partial update" --- it's find if data doesn't contain all the
+   * fields; this only changes provided ones.
+   *
+   * Data can include: { title, salary, equity }
+   *
+   * Returns { id, title, salary, equity, companyHandle }
+   *
+   * Throws NotFoundError if not found.
+   */
+
+  static async update(id, data) {
+    const { setCols, values } = sqlForPartialUpdate(data);
+    const handleVarIdx = "$" + (values.length + 1);
+
+    const querySql = `UPDATE jobs
+                      SET ${setCols}
+                      WHERE id = ${handleVarIdx}
+                      RETURNING id, title, salary, equity, company_handle AS "companyHandle"`;
+    const result = await db.query(querySql, [...values, id]);
+    const job = result.rows[0];
+
+    if (!job) throw new NotFoundError(`No job: ${id}`);
+
+    return job;
+  }
+
+  /** Delete given job from database; returns undefined.
+   *
+   * Throws NotFoundError if company not found.
+   */
+
+  static async remove(id) {
+    const result = await db.query(
+      `DELETE
+      FROM jobs
+      WHERE id = $1
+      RETURNING id`,
+      [id]
+    );
+    const job = result.rows[0];
+
+    if (!job) throw new NotFoundError(`No job: ${id}`);
   }
 }
+
+module.exports = Job;
